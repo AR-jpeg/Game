698c3ab2b96a50ae639647f006fe95326c9bdd39
AR_JPEG <58893337+AR-jpeg@users.noreply.github.com>
2020-06-18 16:56:46 -0500

    Create settings.py

---
+++ settings.py
@@ -0,0 +1,16 @@
+"""Main Settings for the game..."""
+
+
+# For the game
+HEIGHT = 500
+WIDTH = 500
+FPS = 30
+
+# Commen Colors
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+BLUE = (0, 0, 255)
+YELLOW = (255, 255, 0)
+LIGHTBLUE = (0, 155, 155)
